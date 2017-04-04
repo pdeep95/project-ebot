18f78cfbec82e6660acefa1851cca96598ca4069
Pradeep Asundi <pdeep95@gmail.com>
2017-04-04 22:51:05 +0530

    Main Configuration File

--- rpi/main.py
+++ rpi/main.py
@@ -11,7 +11,9 @@ RED_LED, GREEN_LED = 5,6                        # GPIO pin assignment for red & 
 TRIG, ECHO  = 20, 21                            # for distance sensor
 MSDA, MSDL = 2, 3                               # for magnetometer
 
-io.setmode(io.BOARD)
+io.setmode(io.BCM)
+
+io.setwarnings(False)
 
 io.setup(MOTOR1A, io.OUT)               # for Motor
 io.setup(MOTOR1B, io.OUT)
@@ -24,6 +26,7 @@ io.setup(GREEN_LED, io.OUT)
 io.setup(TRIG, io.OUT)                  # for Distance Sensor
 io.setup(ECHO, io.IN)                   # for Distance Sensor
 
+'''
 io.setup(SWITCH, io.IN, pull_up_down=io.PUD_UP)
 
 while True:
@@ -32,3 +35,4 @@ while True:
         # do something when the switch is pressed
         # time.sleep(0.2)
         pass
+'''
