456014296c7f909f8720516b755d05a78d87ff7e
Pradeep Asundi <Pradeep Asundi>
2017-04-04 20:00:39 +0530

    Updated

--- rpi/app/views.py
+++ rpi/app/views.py
@@ -1,12 +1,15 @@
 import os
 import subprocess
 import signal
+import RPi.GPIO as io
 # import psutil
 from django.shortcuts import render
 from django.http import HttpResponse
 from app.models import Process
 # from . import hardware
 
+MOTOR1A, MOTOR1B, MOTOR1E = 18, 23, None        # GPIO pin assignment for motor 1
+MOTOR2A, MOTOR2B, MOTOR2E = 24, 25, None        # GPIO pin assignment for motor 2
 
 def index(request):
     # return HttpResponse("Index Page")
@@ -20,6 +23,10 @@ def navigate(request):
             P = Process(pid=p.pid, direction=dir)
             P.save()
         elif dir == 'stop':
+            io.output(MOTOR1A, False)
+            io.output(MOTOR1B, False)
+            io.output(MOTOR2A, False)
+            io.output(MOTOR2B, False)
             P = Process.objects.latest('id')
             pid = P.pid
             # P.delete()
--- rpi/test.py
+++ rpi/test.py
@@ -1,10 +1,15 @@
 import sys
 import time
 
-print(sys.argv[1])
+print(sys.argv[0])
 
 while True:
     try:
         pass
     except KeyboardInterrupt:
         break
+
+# Popen.poll() - Check if child process has terminated. Set and return returncode attribute.
+# Popen.returncode - The child return code, set by poll() and wait() (and indirectly by communicate()). 
+# A None value indicates that the process has not terminated yet.
+# A negative value -N indicates that the child was terminated by signal N (Unix only).
